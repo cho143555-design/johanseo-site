4c9b75f1960658afd2010072dcb52bc45eb0d39e
cho143555-design <cho143555@gmail.com>
2026-07-21 13:35:42 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // anon 키는 공개용 키라서 노출돼도 안전합니다.
 
 window.SUPABASE_URL = "https://abstebkaqdqhpopxusfg.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFic3RlYmthcWRxaHBvcHh1c2ZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NjU1MjciLCJleHAiOjIxMDAxNDE1Mjd9.EpwiehbuzFTb76sDpAvQCM0CE0jqR0S4cwGUAcddjQs";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFic3RlYmthcWRxaHBvcHh1c2ZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NjU1MjcsImV4cCI6MjEwMDE0MTUyN30.EpwiehbuzFTb76sDpAvQCM0CE0jqR0S4cwGUAcddjQs";
